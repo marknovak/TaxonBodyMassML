54f528f7b53b3abce76c1f5af91ac0e3ffd17fa5
pasquang <pasquang@oregonstate.edu>
2025-11-09 14:48:05 -0800

    Cleaned archaic code and added comments

--- data_partition/data_split_visualization.py
+++ data_partition/data_split_visualization.py
@@ -1,64 +1,44 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib
 from ydata_profiling import ProfileReport
 import seaborn as sns
 import numpy as np
 from sklearn.preprocessing import PowerTransformer
+matplotlib.use('TkAgg')
 
-
-
-
-#separate a dataset into (num_bins) bins and display a histogram based on "mass_g" variable
-#Split each bin into its own dataframe and return as a list
-#inputs: dataframe with "mass_g" variable containing a float of a species mass
-#        num_bins = number of bins to use for histogram
-def print_and_calc(name, df, num_bins):
-    print(df)
-    counts, bin_edges, _ = plt.hist(df["mass_g"], bins=num_bins)
-    plt.title(f"{name} original distribution")
-    plt.xlabel("Mass(g)")
-    plt.ylabel("Count")
-    plt.show()
-    plt.cla()
-    print("Bin counts:", counts)
-    print("Bin edges", bin_edges)
-    df_bins = []
-    for i in range(num_bins):
-        bin_mask = (df["mass_g"] > bin_edges[i]) & (df["mass_g"] <= bin_edges[i + 1])
-        curr_bin = df[bin_mask]
-        df_bins.append(curr_bin)
-        print("Bin", i, ": \n", curr_bin)
-    return df_bins
-
-#split each bin into test and training data based
-def separate_test_train(df_bins, test_train_split_proportion):
-    test = pd.DataFrame()
-    train = pd.DataFrame()
-    for bin in df_bins:
-        if len(bin) == 0:
-            print("Empty bin")
-        else:
-            sample = bin.sample(frac=test_train_split_proportion, replace=False)
-            bin = bin.drop(sample.index)
-            print("Sample:", sample)
-            print("Train:", bin)
-            test = pd.concat([test, sample])
-            train = pd.concat([train, bin])
-
-    return test, train
-            
+#apply z normalization to the mass_g variable in the data       
 def z_normalization(df):
+    
+    #remove rows with missing data 
     print("Number of missing values in each column:\n", df.isna().sum())
     df = df.dropna()
-    df["normalized_mass"] = (df["mass_g"] - df["mass_g"].mean()) / df["mass_g"].std()
+    
+    #normalize mass data using z-normalization
+    df["normalized_mass"] = (df["mass_g"] - df["mass_g"].mean()) / df["mass_g"].std() 
     print(df)
     
+    return df
+    
+def main():
+    #read bodymass data into a pandas dataframe
+    df = pd.read_csv("./data/BodyMass.csv")
+    
+    #create a new variable which is the mass_g variable with z-normalization applied
+    df = z_normalization(df)
+    
+    #ydata profiliing report is saved to my_report.html
+    profile = ProfileReport(df, title="Profiling Report")
+    profile.to_file("my_report.html")
+
+    #create log1p visualization for the mass_g variable
     sns.histplot(np.log1p(df["mass_g"]), bins=50, kde=True)
     plt.title("Log-Transformed mass_g (log1p)")
     plt.ylabel("Count")
     plt.xlabel("Mass (g)")
-    plt.show()
+    plt.show(block=True)
     
+    #use yeo-johnson method to find a transformation that makes visualizing normalized mass variable readable
     pt = PowerTransformer(method='yeo-johnson')
     normalized_mass = df["normalized_mass"].to_numpy()
     transformed = pt.fit_transform(normalized_mass.reshape(-1, 1))
@@ -66,64 +46,20 @@ def z_normalization(df):
     plt.title("Yeo-Johnson Transformed Mass Data")
     plt.ylabel("Count")
     plt.xlabel("Z-Normalized Mass")
-    plt.show()
-    
-    return df
-    
-def main():
-    df = pd.read_csv("./data/BodyMass.csv")
-    df = z_normalization(df)
-    profile = ProfileReport(df, title="Profiling Report")
-    profile.to_file("my_report.html")
-    test = pd.DataFrame()
-    train = pd.DataFrame()
+    plt.show(block=True)
+
+    #test data is a random sample of 10% of the database
     test = df.sample(frac=0.1, replace=False)
+    
+    #train data is the remaining 90%
     train = df.drop(test.index)
+    
     print(test)
     print(train)
+    
+    #save the train/test split to independent csv files
     test.to_csv("test.csv", index=False)
     train.to_csv("train.csv", index=False)
-    """
-    low_bound = 1
-    high_bound = 10000
-    num_bins = 30
-    test_train_split_proportion = 0.1
-    low = df[df["mass_g"] < low_bound]
-    mid = df[df["mass_g"] < high_bound] 
-    mid = mid[mid["mass_g"] >= low_bound] 
-    high = df[df["mass_g"] >= high_bound]
     
-    low_bins = print_and_calc("low", low, num_bins)
-    mid_bins = print_and_calc("mid", mid, num_bins)
-    high_bins = print_and_calc("high", high, num_bins)
-    
-    low_test, low_train = separate_test_train(low_bins, test_train_split_proportion)
-    mid_test, mid_train = separate_test_train(mid_bins, test_train_split_proportion)
-    high_test, high_train = separate_test_train(high_bins, test_train_split_proportion)
-    
-    test = pd.concat([low_test, mid_test, high_test])
-    train = pd.concat([low_train, mid_train, high_train])
-    
-    plt.hist(low_test["mass_g"], bins=num_bins)
-    plt.title("Low test distribution")
-    plt.xlabel("Mass(g)")
-    plt.ylabel("Count")
-    plt.show()
-    plt.cla()
-    
-    plt.hist(mid_test["mass_g"], bins=num_bins)
-    plt.title("Mid test distribution")
-    plt.xlabel("Mass(g)")
-    plt.ylabel("Count")
-    plt.show()
-    plt.cla()
-    
-    plt.hist(high_test["mass_g"], bins=num_bins)
-    plt.title("High test distribution")
-    plt.xlabel("Mass(g)")
-    plt.ylabel("Count")
-    plt.show()
-    plt.cla()
-    """
     
 main()
