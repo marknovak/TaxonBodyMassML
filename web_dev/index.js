5f8e1b883b0bdcfa9011c3ebd4bb7cca62d597a8
praterh <praterh@oregonstate.edu>
2025-11-08 23:58:09 -0800

    feat(ui): added an intro modal and set up placeholder code for microservice implementation

--- web_dev/index.js
+++ web_dev/index.js
@@ -11,24 +11,56 @@ var confidenceOutput = document.getElementById("confidenceOutput")
 var learnMoreArrow = document.getElementById("learnMoreArrow")
 var explanationModal = document.getElementById("explanationModal")
 var goBackButton = document.getElementById("goBackButton")
+var introBox = document.getElementById("introBox")
+var closeIntro = document.getElementById("closeIntro")
 
-function handleGoClick(event){
-	inputBox.classList.add("moved")
-	if (outputBox.classList.contains("hidden")){
-		outputBox.classList.toggle("hidden")
-		learnMoreArrow.classList.toggle("hidden")
+async function handleGoClick(event) {
+	inputBox.classList.add("moved");
+
+	if (outputBox.classList.contains("hidden")) {
+    		outputBox.classList.toggle("hidden");
+    		learnMoreArrow.classList.toggle("hidden");
+  	}
+
+  	const userInput = inputBar.value.trim();
+  	if (!userInput) {
+    		massOutput.textContent = "no input";
+    		return;
+  	}
+
+  	massOutput.textContent = "Checking species name...";
+
+  	try {
+    		// I will replace this with Grant's microservice when it's ready
+   		const data = await myFakeMicroservice(userInput);
+
+   		if (data.status === "success") {
+      			massOutput.textContent = `success: ${data.message}`;
+    		}
+		else {
+      			massOutput.textContent = `not success: ${data.error}`;
+    		}
 	}
+	catch (error) {
+        	console.error(error);
+        	massOutput.textContent = "Error";
+    	}
 
-	const userInput = inputBar.value.trim();
-        if (userInput) {
-                massOutput.textContent = userInput
-                inputBar.value = ''
-        }
-        else {
-                massOutput.textContent = "input something"
-        }
-
+  	inputBar.value = "";
 }
+
+//get rid of this later
+async function myFakeMicroservice(query) {
+  	const speciesList = ["Homo sapiens", "Canis lupus", "Felis catus"];
+
+  	if (speciesList.includes(query)) {
+    		return { status: "success", message: `${query}` };
+ 	 } 	
+	else {
+    		return { status: "error", error: `not found` };
+  	}
+}
+
 
 function handleListClick(event){
         if (csvCheck.classList.contains("hidden")){
@@ -60,6 +92,9 @@ function handleGoBackClick(event){
 	learnMoreArrow.classList.toggle("hidden")
 }
 
+function handleCloseIntro(event){
+	introBox.classList.add("hidden")
+}
 
 goButton.addEventListener("click", handleGoClick)
 inputBar.addEventListener('keypress', (e) => {
@@ -70,5 +105,6 @@ listButton.addEventListener("click", handleListClick)
 singleButton.addEventListener("click", handleSingleClick)
 learnMoreArrow.addEventListener("click", handleLearnMoreClick)
 goBackButton.addEventListener("click", handleGoBackClick)
+closeIntro.addEventListener("click", handleCloseIntro)
 
 
