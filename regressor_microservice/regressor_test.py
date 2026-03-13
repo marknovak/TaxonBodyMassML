547ab2c7d40f62f4a414fdd282d3a3f609dc17d9
pasquang <pasquang@oregonstate.edu>
2026-03-12 21:29:04 -0700

    Added regressor microservice and corresponding testing script

---
+++ regressor_microservice/regressor_test.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import xgboost as xgb
+import pickle
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import requests
+import json
+
+test = pd.read_csv("../data/test.csv")
+x_test = test.drop(["mass_g"], axis=1)
+for INDEX in range(0, 5):
+    print("Expected mass:", test["mass_g"].iloc[INDEX])
+
+    URL = "http://127.0.0.1:8000/xgb_pred_single"
+    payload = {
+        "kingdom": x_test["kingdom"].iloc[INDEX],
+        "phylum": x_test["phylum"].iloc[INDEX],
+        "class": x_test["class"].iloc[INDEX],
+        "order": x_test["order"].iloc[INDEX],
+        "family": x_test["family"].iloc[INDEX],
+        "genus": x_test["genus"].iloc[INDEX],
+        "species": x_test["species"].iloc[INDEX]
+    }
+
+    response = requests.post(URL, json=payload)
+
+    # Check response
+    if response.status_code == 200:
+        data = response.json()
+        print("Prediction Result:")
+        print(json.dumps(data, indent=4))
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
